b3b0f218c3e536579343a15e76fbc29f52d0e993
ckopecky <kopecky12112@gmail.com>
2019-04-29 11:13:42 -0700

    Add basic form to storepicker

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,9 +3,19 @@ import React from 'react';
 
 class StorePicker extends React.Component {
 
+    //sibling elements need to bave one parent component. 
+    //React.Fragment allows for grouping things together without creating a dummy div
+    //comments in JSX are in curly brackets and must be inside returned element.
+
     render(){
         return (
-            <p>Hello!</p>
+            <form className="store-selector">
+                {/* comment - this is a store picker! */}
+                <h2>Please Enter a Store:</h2>
+                <input type="text"required placeholder="Store Name"/>
+                <button type="submit">Visit Store</button>
+            </form>
+            
         )
     }
 }
