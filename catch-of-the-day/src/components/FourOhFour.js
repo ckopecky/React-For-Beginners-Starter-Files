971026be96710a8e63676e9e38a9ee3b72ce3898
ckopecky <kopecky12112@gmail.com>
2019-04-29 13:44:56 -0700

    Handle routes that are not in router

---
+++ catch-of-the-day/src/components/FourOhFour.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FourOhFour = (props) => {
+    return (
+        <div>
+            <h2>404: Not Found!</h2>
+        </div>
+    );
+};
+
+export default FourOhFour;
