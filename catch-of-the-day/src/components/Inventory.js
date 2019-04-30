8c015335b1271ab92cce204eec05638b3297ca3c
ckopecky <kopecky12112@gmail.com>
2019-04-29 17:35:12 -0700

    add inventory component with edit fish form and add fish form

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import AddFishForm from './AddFishForm';
+import EditFishForm from './EditFishForm';
 
 class Inventory extends Component {
     render() {
         return (
             <div className="inventory">
-                Inventory
+                {Object.keys(this.props.fishes).map(key => <EditFishForm index = {key} key={key} updateFish={this.props.updateFish} fish={this.props.fishes[key]}/>)}
+                <AddFishForm addFish ={this.props.addFish}/>
+                <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
             </div>
         );
     }
